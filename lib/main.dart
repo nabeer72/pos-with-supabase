dc1b13325b901c731f972a895a29b80775b8c21d
Nabeer hussain <nabeerhussain7200@gmail.com>
2026-01-26 23:32:25 -0800

    done

--- lib/main.dart
+++ lib/main.dart
@@ -89,7 +89,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Pos',
      
       home: SplashScreen(),
     );
