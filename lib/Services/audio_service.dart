61c306fd7bf65ef5935ba3dcc1cdee9f27f79e42
Nabeer hussain <nabeerhussain7200@gmail.com>
2026-02-05 02:55:34 -0800

    done

--- lib/Services/audio_service.dart
+++ lib/Services/audio_service.dart
@@ -5,14 +5,11 @@ class AudioService {
   static final AudioService _instance = AudioService._internal();
   factory AudioService() => _instance;
   AudioService._internal();
-
   final AudioPlayer _player = AudioPlayer();
-
   Future<void> playScanBeep() async {
     try {
       // Trigger haptic feedback
       await HapticFeedback.lightImpact();
-      
       // Play custom tune from assets
       await _player.stop(); // Stop any currently playing tune
       await _player.play(AssetSource('tune.wav'));
